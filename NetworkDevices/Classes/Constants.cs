71a7320fbf55b17372fcf68a21fa87efcfd0945d
Woutsthew <nekita351@gmail.com>
2022-04-06 16:02:01 +0400

    убавил timeout

--- NetworkDevices/Classes/Constants.cs
+++ NetworkDevices/Classes/Constants.cs
@@ -11,7 +11,7 @@ namespace NetworkDevices.Classes
 
         public static int CheckStatusPing(string address)
         {
-            PingReply reply = new Ping().Send(address);
+            PingReply reply = new Ping().Send(address, 80);
 
             if (reply.Status == IPStatus.Success)
                 return Convert.ToInt32(reply.RoundtripTime);
